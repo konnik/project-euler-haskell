39ca1701bf191bbe0f7c0345e707e03c8e943542
konnik <niklas.konstenius@gmail.com>
2017-01-18 23:24:46 +0100

    Improve problem 6

--- euler6.hs
+++ euler6.hs
@@ -1,4 +1,4 @@
 -- Problem 6
 main = do
-    let answer = (sum [1..100]^2) - (sum [x*x|x<-[1..100]])
+    let answer = sum [1..100]^2 - sum (map (^2) [1..100])
     putStrLn ("Answer: " ++ show answer)
