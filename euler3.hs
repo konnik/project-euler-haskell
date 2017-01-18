cda3e5e8548626bf5871fecc089f560158dead12
konnik <niklas.konstenius@gmail.com>
2017-01-18 22:57:57 +0100

    Remove unused imports

--- euler3.hs
+++ euler3.hs
@@ -1,9 +1,6 @@
 import Data.List (unfoldr)
 import Data.Maybe (listToMaybe)
 
-import Data.List
-import Data.List.Ordered
-
 factors :: Int -> [Int]
 factors n = unfoldr (\ n -> listToMaybe [(x, div n x) | x <- [2..n], mod n x==0]) n
 
